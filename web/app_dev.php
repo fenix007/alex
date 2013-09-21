4c76d25ac6ccf251843d8e5fa02168ad3810acd9
fenix007 <o4kan@ya.ru>
2013-09-21 23:36:08 +0400

    delete extra )

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 if ((isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')))
-    && !$_COOKIE['asdASss1'] === 'asdAewd23d2d')
+    && !$_COOKIE['asdASss1'] === 'asdAewd23d2d'
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
